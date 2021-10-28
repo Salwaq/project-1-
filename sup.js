9f12dceecc68d29a55a3c7f6ceadf8741bb85258
salwa alqurashi <Salwa.Kheder.abt.js@tuwaiq.edu.sa>
2021-10-28 10:18:01 +0300

    + alert

---
+++ sup.js
@@ -0,0 +1,10 @@
+const buttons = document.querySelectorAll(".btn")
+
+
+buttons.forEach(function (button) {
+    button.addEventListener("click", function () {
+        alert(`Thank you for subscribing , we will contact you soon `)
+    })
+
+
+})
